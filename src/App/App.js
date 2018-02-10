0e68c48120b1806bc978a6c6429b1507fa550ad5
Grace Tay <gktaydesigns@gmail.com>
2018-02-09 21:09:32 -0500

    Commented on component structure. About to include scss

--- src/App/App.js
+++ src/App/App.js
@@ -5,13 +5,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        {/* Intro Component
+          Subheading
+          Heading
+          Intro Paragraph
+        Side Nav Component
+          Menu Items
+        Skillset Component
+          [Four sections with header and paragraph, plus class parameter]
+          Languages/Frameworks
+          Programs/SDKs
+          Education
+          Other Skills
+        Experience Component
+          Heading
+          Paragraph with bold title line
+        Published Apps
+          Primary section with Heading
+          Subheading with heading
+        Published Work
+          Primary section with Heading
+          Subheading with heading
+        Footer Component */}
       </div>
     );
   }
